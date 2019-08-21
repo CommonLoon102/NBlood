fac44dfee15733ca299eb9de93e25b5ba6f698fa
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2019-08-21 13:43:15 +0200

    Revert "try4"
    This reverts commit fa410469a8058e877a7a3a6dbf50f68ddb6e39db.

--- source/blood/src/startgtk.game.cpp
+++ source/blood/src/startgtk.game.cpp
@@ -167,7 +167,7 @@ static void on_inicombo_changed(GtkComboBox* combobox, gpointer user_data)
     GtkTreeModel* model;
     GtkTreePath* path;
     char* description;
-    unsigned char value;
+    int value;
     UNREFERENCED_PARAMETER(user_data);
 
     if (gtk_combo_box_get_active_iter(combobox, &iter))
@@ -176,7 +176,7 @@ static void on_inicombo_changed(GtkComboBox* combobox, gpointer user_data)
         gtk_tree_model_get(model, &iter, 0, &description, 1, &value, -1);
         path = gtk_tree_model_get_path(model, &iter);
 
-        unsigned char inii = 0;
+        int inii = 0;
         for (auto fg = pINIChain; fg; fg = fg->pNext)
         {
             if (inii == value)
@@ -401,7 +401,7 @@ static void PopulateForm(unsigned char pgs)
         inilist = GTK_LIST_STORE(gtk_combo_box_get_model(GTK_COMBO_BOX(stwidgets.inicombo)));
         gtk_list_store_clear(inilist);
 
-        unsigned char inii = 0;
+        int inii = 0;
         for (auto fg = pINIChain; fg; fg = fg->pNext)
         {
             char buf[512];
